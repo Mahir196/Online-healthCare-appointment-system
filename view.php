11b4af6f0a0bb8de1e641a615051f8dadc52e954
Rukaiya <ahnattanha562@gmail.com>
2025-03-17 00:54:50 +0600

    first commit

---
+++ view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <fieldset>
+        <h2>Done! You have submitted form.</h2>
+</body>
+</html>
